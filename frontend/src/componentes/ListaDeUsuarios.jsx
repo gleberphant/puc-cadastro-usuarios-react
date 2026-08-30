74fcde45535edf4f5fa5d7c12d13fa52bb43898e
Gleber <gleberphant@gmail.com>
2026-08-30 11:55:38 -0300

    firebas4rlist

--- frontend/src/componentes/ListaDeUsuarios.jsx
+++ frontend/src/componentes/ListaDeUsuarios.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 import { ListarUsuarios } from "../servicos/usuarios.js";
-
+import { db } from "../repositorios/firebase.js";
+import { collection, getDocs } from "firebase/firestore"
 export function ListaDeUsuarios() {
-  const [listaUsuarios, setListaUsuarios] = useState(ListarUsuarios());
+  const [listaUsuarios, setListaUsuarios] = useState([]);
+
+  const getLista = async () => {
+    const lista = []
+    const querySnapshot = await getDocs(collection(db, "usuarios"));
+    querySnapshot.forEach((doc) => {
+      lista.push(doc)
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+    return lista
+  };
+
+  setListaUsuarios(getLista());
 
   return (
     <>
       <div>
         <div style={{ color: "black" }}>
+          <button onClick={getLista}>click</button>
           <table>
             <tbody>
               {listaUsuarios.map((u) => (
